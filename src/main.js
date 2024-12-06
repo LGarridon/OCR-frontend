746d057f59ee271f2d205c20646da955f1991df8
Carlos <Carlos>
2024-12-06 20:00:37 +0100

    #4- instalado "axios"

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
+import axios from 'axios';
 
 const app = createApp(App);
-
+app.config.globalProperties.$axios = axios;
 app.mount('#app');
